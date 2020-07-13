d33063fd567463d467366cea8addfc217f905b1d
Suwako233 <2422965404@qq.com>
2020-07-13 15:24:59 +0800

    Edit enterprise project 2020年7月13日15:24:45

--- EnterpriseForumProject/src/main/java/com/pojo/BbsReplyInfo.java
+++ EnterpriseForumProject/src/main/java/com/pojo/BbsReplyInfo.java
@@ -3,97 +3,95 @@ package com.pojo;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-
 import java.sql.Date;
-import java.sql.Timestamp;
 
 @TableName("bbs_reply_info")
-public class BbsReplyInfo {
-    @TableId(type = IdType.AUTO)
-    private Integer rId;
-    private Integer rTId;
-    private Integer rSId;
-    private Integer rUId;
-    private String rTopic;
-    private String rContents;
-    private Date rPublishTime;
-    private Date rModifyTime;
+public class BbsReplyInfo  {
+	@TableId(type = IdType.AUTO)
+	private Integer rId;
+	private Integer rTId;
+	private Integer rSId;
+	private Integer rUId;
+	private String rTopic;
+	private String rContents;
+	private Date rPublishTime;
+	private Date rModifyTime;
 
-    public Integer getRId() {
-        return rId;
-    }
+	public Integer getRId() {
+		return rId;
+	}
 
-    public void setRId(Integer rId) {
-        this.rId = rId;
-    }
+	public void setRId(Integer rId) {
+		this.rId = rId;
+	}
 
-    public Integer getRTId() {
-        return rTId;
-    }
+	public Integer getRTId() {
+		return rTId;
+	}
 
-    public void setRTId(Integer rTId) {
-        this.rTId = rTId;
-    }
+	public void setRTId(Integer rTId) {
+		this.rTId = rTId;
+	}
 
-    public Integer getRSId() {
-        return rSId;
-    }
+	public Integer getRSId() {
+		return rSId;
+	}
 
-    public void setRSId(Integer rSId) {
-        this.rSId = rSId;
-    }
+	public void setRSId(Integer rSId) {
+		this.rSId = rSId;
+	}
 
-    public Integer getRUId() {
-        return rUId;
-    }
+	public Integer getRUId() {
+		return rUId;
+	}
 
-    public void setRUId(Integer rUId) {
-        this.rUId = rUId;
-    }
+	public void setRUId(Integer rUId) {
+		this.rUId = rUId;
+	}
 
-    public String getRTopic() {
-        return rTopic;
-    }
+	public String getRTopic() {
+		return rTopic;
+	}
 
-    public void setRTopic(String rTopic) {
-        this.rTopic = rTopic;
-    }
+	public void setRTopic(String rTopic) {
+		this.rTopic = rTopic;
+	}
 
-    public String getRContents() {
-        return rContents;
-    }
+	public String getRContents() {
+		return rContents;
+	}
 
-    public void setRContents(String rContents) {
-        this.rContents = rContents;
-    }
+	public void setRContents(String rContents) {
+		this.rContents = rContents;
+	}
 
-    public Date getRPublishTime() {
-        return rPublishTime;
-    }
+	public Date getRPublishTime() {
+		return rPublishTime;
+	}
 
-    public void setRPublishTime(Date rPublishTime) {
-        this.rPublishTime = rPublishTime;
-    }
+	public void setRPublishTime(Date rPublishTime) {
+		this.rPublishTime = rPublishTime;
+	}
 
-    public Date getRModifyTime() {
-        return rModifyTime;
-    }
+	public Date getRModifyTime() {
+		return rModifyTime;
+	}
 
-    public void setRModifyTime(Date rModifyTime) {
-        this.rModifyTime = rModifyTime;
-    }
+	public void setRModifyTime(Date rModifyTime) {
+		this.rModifyTime = rModifyTime;
+	}
 
-    @Override
-    public String toString() {
-        return "BbsReplyInfo{" +
-                "rId=" + rId +
-                ", rTId=" + rTId +
-                ", rSId=" + rSId +
-                ", rUId=" + rUId +
-                ", rTopic='" + rTopic + '\'' +
-                ", rContents='" + rContents + '\'' +
-                ", rPublishTime=" + rPublishTime +
-                ", rModifyTime=" + rModifyTime +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "BbsReplyInfo{" +
+				"rId=" + rId +
+				", rTId=" + rTId +
+				", rSId=" + rSId +
+				", rUId=" + rUId +
+				", rTopic='" + rTopic + '\'' +
+				", rContents='" + rContents + '\'' +
+				", rPublishTime=" + rPublishTime +
+				", rModifyTime=" + rModifyTime +
+				'}';
+	}
 }
--- EnterpriseForumProject/src/main/java/com/pojo/BbsTopicInfo.java
+++ EnterpriseForumProject/src/main/java/com/pojo/BbsTopicInfo.java
@@ -3,107 +3,105 @@ package com.pojo;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-
 import java.sql.Date;
-import java.sql.Timestamp;
 
 @TableName("bbs_topic_info")
-public class BbsTopicInfo {
-    @TableId(type = IdType.AUTO)
-    private Integer tId;
-    private Integer tSId;
-    private Integer tUId;
-    private String tTopic;
-    private String tContents;
-    private Integer tReplyCount;
-    private Integer tClickCount;
-    private Date tPublishTime;
-    private Date tModifyTime;
+public class BbsTopicInfo  {
+	@TableId(type = IdType.AUTO)
+	private Integer tId;
+	private Integer tSId;
+	private Integer tUId;
+	private String tTopic;
+	private String tContents;
+	private Integer tReplyCount;
+	private Integer tClickCount;
+	private Date tPublishTime;
+	private Date tModifyTime;
 
-    public Integer getTId() {
-        return tId;
-    }
+	public Integer getTId() {
+		return tId;
+	}
 
-    public void setTId(Integer tId) {
-        this.tId = tId;
-    }
+	public void setTId(Integer tId) {
+		this.tId = tId;
+	}
 
-    public Integer getTSId() {
-        return tSId;
-    }
+	public Integer getTSId() {
+		return tSId;
+	}
 
-    public void setTSId(Integer tSId) {
-        this.tSId = tSId;
-    }
+	public void setTSId(Integer tSId) {
+		this.tSId = tSId;
+	}
 
-    public Integer getTUId() {
-        return tUId;
-    }
+	public Integer getTUId() {
+		return tUId;
+	}
 
-    public void setTUId(Integer tUId) {
-        this.tUId = tUId;
-    }
+	public void setTUId(Integer tUId) {
+		this.tUId = tUId;
+	}
 
-    public String getTTopic() {
-        return tTopic;
-    }
+	public String getTTopic() {
+		return tTopic;
+	}
 
-    public void setTTopic(String tTopic) {
-        this.tTopic = tTopic;
-    }
+	public void setTTopic(String tTopic) {
+		this.tTopic = tTopic;
+	}
 
-    public String getTContents() {
-        return tContents;
-    }
+	public String getTContents() {
+		return tContents;
+	}
 
-    public void setTContents(String tContents) {
-        this.tContents = tContents;
-    }
+	public void setTContents(String tContents) {
+		this.tContents = tContents;
+	}
 
-    public Integer getTReplyCount() {
-        return tReplyCount;
-    }
+	public Integer getTReplyCount() {
+		return tReplyCount;
+	}
 
-    public void setTReplyCount(Integer tReplyCount) {
-        this.tReplyCount = tReplyCount;
-    }
+	public void setTReplyCount(Integer tReplyCount) {
+		this.tReplyCount = tReplyCount;
+	}
 
-    public Integer getTClickCount() {
-        return tClickCount;
-    }
+	public Integer getTClickCount() {
+		return tClickCount;
+	}
 
-    public void setTClickCount(Integer tClickCount) {
-        this.tClickCount = tClickCount;
-    }
+	public void setTClickCount(Integer tClickCount) {
+		this.tClickCount = tClickCount;
+	}
 
-    public Date getTPublishTime() {
-        return tPublishTime;
-    }
+	public Date getTPublishTime() {
+		return tPublishTime;
+	}
 
-    public void setTPublishTime(Date tPublishTime) {
-        this.tPublishTime = tPublishTime;
-    }
+	public void setTPublishTime(Date tPublishTime) {
+		this.tPublishTime = tPublishTime;
+	}
 
-    public Date getTModifyTime() {
-        return tModifyTime;
-    }
+	public Date getTModifyTime() {
+		return tModifyTime;
+	}
 
-    public void setTModifyTime(Date tModifyTime) {
-        this.tModifyTime = tModifyTime;
-    }
+	public void setTModifyTime(Date tModifyTime) {
+		this.tModifyTime = tModifyTime;
+	}
 
-    @Override
-    public String toString() {
-        return "BbsTopicInfo{" +
-                "tId=" + tId +
-                ", tSId=" + tSId +
-                ", tUId=" + tUId +
-                ", tTopic='" + tTopic + '\'' +
-                ", tContents='" + tContents + '\'' +
-                ", tReplyCount=" + tReplyCount +
-                ", tClickCount=" + tClickCount +
-                ", tPublishTime=" + tPublishTime +
-                ", tModifyTime=" + tModifyTime +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "BbsTopicInfo{" +
+				"tId=" + tId +
+				", tSId=" + tSId +
+				", tUId=" + tUId +
+				", tTopic='" + tTopic + '\'' +
+				", tContents='" + tContents + '\'' +
+				", tReplyCount=" + tReplyCount +
+				", tClickCount=" + tClickCount +
+				", tPublishTime=" + tPublishTime +
+				", tModifyTime=" + tModifyTime +
+				'}';
+	}
 }
